d843a38732cf931a163972eb746c3ce07d72d7be
Yeong-Ju Kwak <yeongju.kwak@gmail.com>
2022-12-14 21:39:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,21 +4,27 @@ ARCH_NAME=$(arch)
 
 if [[ $ARCH_NAME == "i386" ]]
 then
-        echo
+        echo 
+        echo 
+        echo "===================="
         echo "> Homebrew Install.."
+        echo "===================="
         arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         
-        echo "###### Settings required to install Python ######" >> ~/ .zshrc
-        
-        # brew86
-        echo 'alias brew86="arch -x86_64 /usr/local/bin/brew"' >> ~/.zshrc
-        sleep 5
-        source ~/.zshrc
-        sleep 5
-
-        echo
+        echo 
+        echo 
+        echo "================================================"
         echo "> pyenv gcc zlib bzip2 ncurses SQLite3 Install.."
-        brew86 install pyenv gcc zlib bzip2 ncurses SQLite3
+        echo "================================================"
+        arch -x86_64 /usr/local/bin/brew install pyenv gcc zlib bzip2 ncurses SQLite3
+        
+        GCC_NAME=$(ls -al /usr/local/bin | awk "{print $9}" | grep "^gcc-[0-9]")
+        
+        echo "###### Settings required to install Python ######" >> ~/.zshrc
+        
+        # alias
+        echo 'alias brew86="arch -x86_64 /usr/local/bin/brew"' >> ~/.zshrc
+        echo 'alias pyenv86="CC=`which $GCC_NAME` pyenv"' >> ~/.zshrc
         
         # zlib
         echo 'export LDFLAGS="-L/usr/local/opt/zlib/lib $LDFLAGS"' >> ~/.zshrc
@@ -38,31 +44,29 @@ then
         echo 'export PATH="/usr/local/opt/sqlite/bin:$PATH"' >> ~/.zshrc
         echo 'export LDFLAGS="-L/usr/local/opt/sqlite/lib $LDFLAGS"' >> ~/.zshrc
         echo 'export CPPFLAGS="-I/usr/local/opt/sqlite/include $CPPFLAGS"' >> ~/.zshrc
-                
-        echo "#################################################" >> ~/.zshrc
-
-        # pyenv86
-        GCC_NAME=$(ls -al /usr/local/bin | awk "{print $9}" | grep "^gcc-[0-9]")
-        echo 'alias pyenv86="CC=`which $GCC_NAME` pyenv"' >> ~/.zshrc
-        sleep 5
-        source ~/.zshrc
-        sleep 5
         
+        echo "#################################################" >> ~/.zshrc
+        source ~/.zshrc
+
+        echo 
+        echo 
         echo "=============== Info ==============="
         echo "> Version Info."
-        echo "Homebrew $(brew86 --version)"
+        echo "Homebrew $/usr/local/bin/brew --version)"
         echo "pyenv $(pyenv --version)"
         echo
         echo "> How to install Python. (pyenv86 install {Python Version})"
-        echo "ex) pyenv86 install 3.5.3"
+        echo "ex) pyenv86 install 3.7.7"
         echo 
         echo "Made by YeongJu-Kwak"
         echo "===================================="
-        echo
+        echo 
         echo "Finished."
 else
+        echo 
         echo "===================================="
         echo "ERR ) arch: $ARCH_NAME"
         echo "Hint) Open a terminal using Rosetta."
         echo "===================================="
+        echo 
 fi
